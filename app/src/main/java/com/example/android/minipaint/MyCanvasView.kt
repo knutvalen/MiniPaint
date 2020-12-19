68cba36b2b1efb0bf010fc91b72af583a94d11df
Knut Valen <knut.valen@icloud.com>
2020-12-19 13:04:44 +0100

    add canvas

--- app/src/main/java/com/example/android/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/example/android/minipaint/MyCanvasView.kt
@@ -3,4 +3,6 @@ package com.example.android.minipaint
 import android.content.Context
 import android.view.View
 
-class MyCanvasView(context: Context) : View(context) {}
+class MyCanvasView(context: Context) : View(context) {
+
+}
